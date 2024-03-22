1cad19dad162650970ca79c58affc5571b60c15b
JuliusFaj <funnsojulius16@gmail.com>
2024-03-21 19:39:07 -0700

    first push

---
+++ lib/screens/onBoarding.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.light,
+        systemNavigationBarColor: Theme
+            .of(context)
+            .colorScheme
+            .background,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+        child: const Scaffold(
+          body: Center(
+            child: Text('hii'),
+          )
+        ),
+    );
+  }
+}
