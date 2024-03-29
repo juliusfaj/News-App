242a72304d2652e543286587cf6a146189d128ff
JuliusFaj <funnsojulius16@gmail.com>
2024-03-28 23:49:29 -0700

    code update

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -6,9 +6,46 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
-      ),
+      body: Column(
+        children: [
+           const SizedBox(height: 20,),
+
+          Expanded(
+            child: PageView(
+                  scrollDirection: Axis.horizontal,
+                  controller: PageController(viewportFraction: .86),
+                  children: [
+                  Stack(
+                   children: [
+                     Container(
+                       height: 200,
+                       width: 300,
+                       decoration: const BoxDecoration(
+                         image: DecorationImage(
+                           image: AssetImage('assets/newsImg.jpg'),
+                           fit: BoxFit.cover,
+                         ),
+                       ),
+                     ),
+
+                     FilledButton(
+                         onPressed: (){},
+                       style: ElevatedButton.styleFrom(
+                            backgroundColor: Theme.of(context).colorScheme.onPrimary,
+                            foregroundColor: Theme.of(context).colorScheme.primary,
+                         maximumSize: const Size(150, 50),
+                         ),
+                         child: const Text('Environment'),
+                     ),
+                   ],
+                  ),
+
+                  ],
+                ),
+          ),
+        ],
+      )
+
     );
   }
 }
