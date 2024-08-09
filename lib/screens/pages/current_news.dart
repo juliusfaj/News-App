5e6a2620fa5d90d71dc4d1874a0de3d4ad43d48d
Julius <funnsojulius16@gmail.com>
2024-08-09 10:31:03 +0100

    code update

--- lib/screens/pages/current_news.dart
+++ lib/screens/pages/current_news.dart
@@ -16,54 +16,72 @@ class CurrentNews extends StatelessWidget {
           ),
           onPressed: (){Get.back();},
         ),
-        expandedHeight: 600,
+        expandedHeight: 500,
         body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
           decoration: BoxDecoration(
             color: Theme.of(context).colorScheme.scrim,
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 20,),
-              const Text(
-                'Title',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
+          child: SizedBox(
+            height: 1200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20,),
+                const Text(
+                  'Title',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20,),
-              Row(
-                children: [
-                  Container(
-                    height: 30,
-                    width: 30,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary.withOpacity(.5),
-                      shape: BoxShape.circle,
+                const SizedBox(height: 20,),
+                Row(
+                  children: [
+                    Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary.withOpacity(.5),
+                        shape: BoxShape.circle,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10,),
-                  Text('John Doe', style: TextStyle(color:Colors.grey.withOpacity(.8), fontWeight: FontWeight.bold),),
-                  const SizedBox(width: 10,),
-                  Container(
-                    height: 7,
-                    width: 7,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondary.withOpacity(.5),
-                      shape: BoxShape.circle,
+                    const SizedBox(width: 10,),
+                    Text('John Doe', style: TextStyle(color:Colors.grey.withOpacity(.8), fontWeight: FontWeight.bold),),
+                    const SizedBox(width: 10,),
+                    Container(
+                      height: 7,
+                      width: 7,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.secondary.withOpacity(.5),
+                        shape: BoxShape.circle,
+                      ),
                     ),
+                    const SizedBox(width: 20,),
+                    Text('Feb 27, 2023',
+                      style: TextStyle(
+                          color:Colors.grey.withOpacity(.8),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20,),
+                Container(
+                  width: double.infinity,
+                  height: 2,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(.5),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  const SizedBox(width: 20,),
-                  Text('Feb 27, 2023',
-                    style: TextStyle(
-                        color:Colors.grey.withOpacity(.8),
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              )
-            ],
+                ),
+                const SizedBox(height: 30,),
+                Text('est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerumnecessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maximeplaceat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                ),)
+              ],
+            ),
           ),
         ),
         header: Stack(
